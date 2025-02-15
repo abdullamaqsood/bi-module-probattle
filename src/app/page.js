69c1e27a265c5659b5b0925fa272408c348ac4ae
Abdullah Maqsood <abdullahmaqsood2002@gmail.com>
2025-02-15 14:01:56 +0500

    link removed

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,11 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [datasetLink, setDatasetLink] = useState("https://www.kaggle.com/datasets/omercolakoglu/turkish-market-sales-dataset-with-9000items/data");
+  const [datasetLink, setDatasetLink] = useState("");
   const [submissionLink, setSubmissionLink] = useState(
     "https://forms.gle/u39iKnW6eYHqDehc7"
   );
-  //
+  //https://www.kaggle.com/datasets/omercolakoglu/turkish-market-sales-dataset-with-9000items/data
   return (
     <div className="bg-gray-100">
       {/* Sticky Navbar */}
